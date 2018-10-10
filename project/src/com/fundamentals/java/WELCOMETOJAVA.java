ba1e1a352da6d6fa098d50fa848325b1692ea8d2
D'vonte Brown <brodvo19@gmail.com>
2018-10-10 11:17:14 -0500

    this is our first java project

---
+++ project/src/com/fundamentals/java/WELCOMETOJAVA.java
@@ -0,0 +1,10 @@
+package com.fundamentals.java;
+
+public class WELCOMETOJAVA {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("welcometojava");
+	}
+
+}
