e139c4fc9d368a5b6369a2e7ae265563b8f94a6a
D'vonte Brown <brodvo19@gmail.com>
2018-10-10 15:42:34 -0500

    My first method & commits

--- project/src/com/fundamentals/java/WELCOMETOJAVA.java
+++ project/src/com/fundamentals/java/WELCOMETOJAVA.java
@@ -1,10 +1,27 @@
 package com.fundamentals.java;
 
+//public - Access Modifier 
+//class - file type
 public class WELCOMETOJAVA {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("welcometojava");
-	}
+		someMethod();
 
+	}
+//	my first method 
+public static void someMethod() {
+	//this is a single comment
+	/**
+	 * this
+	 * is a multi-line comment
+	 **/
+	System.out.println("welcometojava");	
+	
 }
+}
+
+
+
+
+
